10368b3cd9fdab12f2948ce2b78035b57625e346
EricBR <eric.bedoyar@uqvirtual.edu.co>
2024-05-26 21:34:49 -0500

    correccion integracion

--- src/main/java/co/org/uniquindio/quiz/dto/Docente/DocenteUpdateDTO.java
+++ src/main/java/co/org/uniquindio/quiz/dto/Docente/DocenteUpdateDTO.java
@@ -1,25 +1,15 @@
 package co.org.uniquindio.quiz.dto.Docente;
 
-<<<<<<< HEAD
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 public record DocenteUpdateDTO (
 
-    @NotNull long idDocente,
-=======
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
-public record DocenteUpdateDTO (
-
-    @NotNull  long idDocente,
->>>>>>> 84258cb19c6f1f3f98a8a2e51aef6262180a0cc8
-    @NotNull  @Email String correo,
-    @NotNull  String passwordCuenta,
-    @NotNull  String cedulaDocente,
-    @NotNull  String nombreDocente
+        @NotNull long idDocente,
+        @NotNull  @Email String correo,
+        @NotNull  String passwordCuenta,
+        @NotNull  String cedulaDocente,
+        @NotNull  String nombreDocente
 )
 
 {}
--- src/main/java/co/org/uniquindio/quiz/servicios/impl/DocenteServicioImpl.java
+++ src/main/java/co/org/uniquindio/quiz/servicios/impl/DocenteServicioImpl.java
@@ -3,19 +3,12 @@ package co.org.uniquindio.quiz.servicios.impl;
 import co.org.uniquindio.quiz.dto.Docente.DocenteUpdateDTO;
 import co.org.uniquindio.quiz.repositorios.DocenteRepo;
 import co.org.uniquindio.quiz.servicios.interfaces.DocenteServicio;
-<<<<<<< HEAD
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-=======
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
->>>>>>> 84258cb19c6f1f3f98a8a2e51aef6262180a0cc8
 
 @Service
 @Transactional
